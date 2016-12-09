ce9af58bb0e8974e8d6da6be6949f89ee918c916
angiecrellin <angiecrellin@gmail.com>
2016-12-09 15:35:56 -0600

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Card = require('./models/flashcards')
 
-mongoose.connect('mongodb://<angiecrellin>:<Tex78620>@ds127998.mlab.com:27998/flashcardsapp')
+mongoose.connect('mongodb://angiecrellin:Cal92683@ds127998.mlab.com:27998/flashcardsapp')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,31 +13,35 @@ const port = 8080
 
 app.post('/create-card', (req, res) => {
     let card = new Card()
-    card.term = req.body.term
-    card.definition = req.body.definition
+    card.term = 'CSS'
+    card.definition = 'Cascading Style Sheets'
+    card.save(err => {
+      if (err)
+          res.send(err)
+          res.json({ msg:'saved'})
+
+  })
 })
 
+app.get('/cards', (req, res) => {
+  Card.find((err, cards) => {
+          if (err)
+              res.send(err);
 
-/*Why not route folder with api.js file and this kind of thing. 
-Then in server.js app.use('/api/, apiRoutes') and runServer function?
+          res.json(cards);
+      })
+})
 
-router.post('/cards', function(request, response) {
-    if (!request.body.name) {
-        return response.status(400).json({
-            message: 'no data sent'
-        });
-    }
-    Card.create({
-        term: request.body.term,
-        definition: request.body.definition,
-    }, function(err, recipes) {
-        if (err) {
-            return response.status(500).json({
-                message: 'Internal Server Error'
-            });
-        }
-        response.status(201).json(recipes);
-    });
-});*/
+app.delete('/cards/:id', (req, res)  => {
+    Card.find((err, _id) => {
+          if (err)
+            res.send(err);
 
-app.listen(port);
+          res.json({ msg: 'card deleted'})
+    })
+
+})
+
+app.listen(port, () => {
+  console.log('server is listening woohoo')
+});
