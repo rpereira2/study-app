8c66fc70c94947cd053981365ef0e2287874053e
angiecrellin <angiecrellin@gmail.com>
2016-12-20 18:27:03 -0600

    add README and documentation

--- models/flashcards.js
+++ models/flashcards.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const CardSchema = new Schema({
     term: String,
-    definition: String 
+    definition: String
 });
 
 module.exports = mongoose.model('Card', CardSchema)
